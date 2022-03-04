aac81a94f3d203c0258a1993348ef20b81bed53c
Codemaster-69 <fredrik.lillejord@gmail.com>
2022-03-04 13:08:50 +0100

    - Organise files into folders - Add content model - Set content model as environment object - Rename content view

---
+++ Shared/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  Learning App (iOS)
+//
+//  Created by Fredrik Raknes Lillejord on 04/03/2022.
+//
+
+import Foundation
+
+
+class ContentModel: ObservableObject {
+    
+    
+}
